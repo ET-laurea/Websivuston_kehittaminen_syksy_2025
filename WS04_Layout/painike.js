8a14bb4a995b1ecbc95dcdf8ecbc0bc13c4fdd5e
Erno <ert00012@laurea.fi>
2025-11-15 15:41:27 +0200

    8th commit

---
+++ WS04_Layout/painike.js
@@ -0,0 +1,6 @@
+'use strict';
+
+document.getElementById("zoomButton").addEventListener("click", function() {
+    const img = document.getElementById("kuva");
+    img.width += 200;  // kasvattaa kuvaa 50px
+});
